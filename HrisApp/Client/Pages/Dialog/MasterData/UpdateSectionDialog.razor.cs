5f6381ba4a75e3606fe6bb0d611dd2b5c530fe81
JM Mabasa <JM Mabasa@DESKTOP-6LTROHR>
2023-10-14 11:51:51 +0800

    fix somethings

---
+++ HrisApp/Client/Pages/Dialog/MasterData/UpdateSectionDialog.razor.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.JSInterop;
+
+namespace HrisApp.Client.Pages.Dialog.MasterData
+{
+    public partial class UpdateSectionDialog : ComponentBase
+    {
+
+#nullable disable
+        [CascadingParameter] MudDialogInstance MudDialog { get; set; }
+        [Parameter]
+        public int Id { get; set; }
+
+
+        private SectionT section = null;
+
+
+        void Cancel() => MudDialog.Cancel();
+
+
+        protected override async Task OnParametersSetAsync()
+        {
+            section = SectionService.SectionTs.Find(d => d.Id == Id);
+        }
+
+        async Task UpdateArea()
+        {
+            if (section == null)
+                return;
+
+            MudDialog.Close();
+
+            var confirmResult = await Swal.FireAsync(new SweetAlertOptions
+            {
+                Title = "Confirmation",
+                Text = "Are you sure you want to update the " + section.Name + "?",
+                Icon = SweetAlertIcon.Question,
+                ShowCancelButton = true,
+                ConfirmButtonText = "Yes",
+                CancelButtonText = "No"
+            });
+
+            if (confirmResult.IsConfirmed)
+            {
+                await SectionService.UpdateSection(section);
+                await Swal.FireAsync(new SweetAlertOptions
+                {
+                    Text = section.Name + " updated Successfully!",
+                    Icon = SweetAlertIcon.Success
+                });
+                await jsRuntime.InvokeVoidAsync("location.reload");
+                navigationManager.NavigateTo("/section");
+            }
+        }
+    }
+}
---
+++ HrisApp/Client/Pages/MasterData/Section.razor.cs
@@ -0,0 +1,63 @@
+﻿namespace HrisApp.Client.Pages.MasterData
+{
+    public partial class Section : ComponentBase
+    {
+        private List<DepartmentT> Departments = new List<DepartmentT>();
+        private List<DivisionT> Divisions = new List<DivisionT>();
+        private List<SectionT> Sections = new List<SectionT>();
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                await DepartmentService.GetDepartment();
+                await DivisionService.GetDivision();
+                await SectionService.GetSection();
+
+                Divisions = DivisionService.DivisionTs;
+                Departments = DepartmentService.DepartmentTs;
+                sectionList = SectionService.SectionTs;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void OpenAddSection()
+        {
+            var options = new DialogOptions { CloseOnEscapeKey = true };
+            DialogService.Show<AddSectionDialog>("New Section", options);
+        }
+
+        private void OpenUpdateSection(int id)
+        {
+            var parameters = new DialogParameters<UpdateSectionDialog>();
+            parameters.Add(x => x.Id, id);
+
+            var options = new DialogOptions { CloseOnEscapeKey = true };
+            DialogService.Show<UpdateSectionDialog>("Update Section", parameters, options);
+        }
+
+        //TABLEEES
+        private string infoFormat = "{first_item}-{last_item} of {all_items}";
+        private string searchString1 = "";
+        List<SectionT> sectionList = new List<SectionT>();
+
+        private SectionT selectedItem1 = null;
+        private HashSet<SectionT> selectedItems = new HashSet<SectionT>();
+
+        private bool FilterFunc1(SectionT section) => FilterFunc(section, searchString1);
+
+        private bool FilterFunc(SectionT section, string searchString)
+        {
+            if (string.IsNullOrWhiteSpace(searchString))
+                return true;
+            if (section.Name.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+                return true;
+            return false;
+        }
+        //END FOR TABLES
+    }
+}
