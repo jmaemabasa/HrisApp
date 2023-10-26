393190c28aabf6326f27a7abf83c75efd1dfe506
jmaetogonon <jochellemae16@gmail.com>
2023-10-27 00:22:35 +0800

    after sa balay

--- HrisApp/Shared/Models/User/UserLoginDto.cs
+++ HrisApp/Shared/Models/User/UserLoginDto.cs
@@ -17,7 +17,7 @@ namespace HrisApp.Shared.Models.User
         [Required]
         [EmailAddress]
         public string Email { get; set; } = string.Empty;
-        [Required, StringLength(100, MinimumLength = 6)]
+        //[Required, StringLength(100, MinimumLength = 6)]
         public string Password { get; set; } = string.Empty;
         [Required]
         [Compare("Password", ErrorMessage = "The passwords do not match.")]
